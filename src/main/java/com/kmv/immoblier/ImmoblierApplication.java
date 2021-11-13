ae9a90f8d76115399f04aeac4043e505c297b0be
omarkourmou <kourmou.omar@gmail.com>
2021-11-13 13:20:08 +0100

    {omarkourmou}{init back gestion annonce immobiler}

---
+++ src/main/java/com/kmv/immoblier/ImmoblierApplication.java
@@ -0,0 +1,13 @@
+package com.kmv.immoblier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmoblierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImmoblierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kmv/immoblier/ImmoblierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmv.immoblier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmoblierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
